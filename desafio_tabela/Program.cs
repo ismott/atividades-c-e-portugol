974fbf7a410e6c2a9a52e0232e9de814f3d21ab9
ismot <israel.j.mota@hotmail.com>
2021-05-06 18:07:27 -0300

    array e foreatch

---
+++ desafio_tabela/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace desafio_tabela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] produtos;
+            Console.WriteLine("quantos produtos gostaria de cadastrar?");
+            int quantidadesProdutos = int.Parse(Console.ReadLine());
+            produtos = new int[quantidadesProdutos];
+
+            string[] nomeProdutos;
+            nomeProdutos = new string[quantidadesProdutos];
+            
+            for (var i = 0; i < quantidadesProdutos; i++)
+            {
+                Console.WriteLine($"qual o nome do {i + 1}º produto");
+                nomeProdutos[i] = Console.ReadLine();    
+            }
+
+            float[] valorProduto;
+            valorProduto = new float[quantidadesProdutos];
+            for (var i = 0; i < quantidadesProdutos; i++)
+            {
+                Console.WriteLine($"quato custara o {i + 1}º produto");
+                valorProduto[i] = float.Parse(Console.ReadLine());  
+            }
+
+            int[] quantiadeCadaProduto;
+            quantiadeCadaProduto = new int[quantidadesProdutos];
+            for (var i = 0; i < quantidadesProdutos; i++)
+            {
+                Console.WriteLine($"qual a quantidade do {i + 1}º produto");
+                quantiadeCadaProduto[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($@"=======================================================
+|      NOME      |     QUANTIDADE     |     VALOR     |
+=======================================================
+            "); 
+
+            for (var i = 0; i < quantidadesProdutos; i++)
+            {
+                Console.WriteLine($@"|      {nomeProdutos[i]}       |         {quantiadeCadaProduto[i]}          |       {valorProduto[i]}       |                                   
+                ");
+            }
+            
+            Console.WriteLine(@"=======================================================");
+               
+        }
+    }
+}
---
+++ for_each/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace for_each
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] embalagens = null;
+
+            Console.WriteLine("quantas embalagens de pão de queijo tem na pratileira?");
+            int quantidadeEmbalagens = int.Parse(Console.ReadLine());
+            embalagens = new int [quantidadeEmbalagens];
+
+            int c = 0;
+            foreach (var item in embalagens)
+            {
+                Console.WriteLine($"quantos pães de queijo tem na {c + 1}º embalagem");
+                embalagens[c] = int.Parse(Console.ReadLine());
+                c++;
+            }
+            int a = 1;
+            foreach (var item in embalagens)
+            {
+                Console.WriteLine($"Na {a}º embalagens a: {item}");
+                a++; 
+            }
+        }
+    }
+}
